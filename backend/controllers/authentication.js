f7de06e23df3329074e6c94dc1ff601b0d565521
RyanHPaez <solosnail@gmail.com>
2022-06-25 19:02:14 -0700

    adding jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,45 +1,59 @@
-const router = require('express').Router()
-const db = require("../models")
-const bcrypt = require('bcrypt')
-const jwt = require('jwt')   
+const router = require("express").Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
+//the guide says to use jwt, instead use json-web-token :npm install json-web-token
+const jwt = require("json-web-token");
 
-const { User } = db
+const { User } = db;
 
-router.post('/', async(req,res)=>{
-    let user = await User.findOne({
-        where: {email: req.body.email}
-    })
-    //did not get 404 but got message with incorrect credentials
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({
-        message: `Could not find a user with the provided username and password` 
-      })
-    } else {
-        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })           
-        res.json({ user: user, token: result.value })           
-       }
-    })
+router.post("/", async (req, res) => {
+  let user = await User.findOne({
+    where: { email: req.body.email },
+  });
+  //did not get 404 but got message with incorrect credentials
+  if (
+    !user ||
+    !(await bcrypt.compare(req.body.password, user.passwordDigest))
+  ) {
+    res.status(404).json({
+      message: `Could not find a user with the provided username and password`,
+    });
+  } else {
+    //this is keeping me signed in
+    const result = await jwt.encode(process.env.JWT_SECRET, {
+      id: user.userId,
+    });
+    //this is where you have acces to logged in user
+    res.json({ user: user, token: result.value });
+  }
+});
+//here is where that fetch request is responding in Curretuser.js
+router.get("/profile", async (req, res) => {
+  try {
+    // Split the authorization header into [ "Bearer", "TOKEN" ]:
+    // extract the JWT from the request headers and decode it to get the ID of the logged-in user
+    const [authenticationMethod, token] = req.headers.authorization.split(" ");
 
-router.get('/profile', async (req, res) => {
-    try {
-        let user = await User.findOne({
-            where: {
-                // userId:  
-            }
-        })
-        res.json(user)
-    } catch {
-        res.json(null)
+    // Only handle "Bearer" authorization for now
+    //  (we could add other authorization strategies later):
+    if (authenticationMethod == "Bearer") {
+      // Decode the JWT
+      const result = await jwt.decode(process.env.JWT_SECRET, token);
+
+      // Get the logged in user's id from the payload
+      const { id } = result.value;
+
+      // Find the user object using their id:
+      let user = await User.findOne({
+        where: {
+          userId: id,
+        },
+      });
+      res.json(user);
     }
-})
+  } catch {
+    res.json(null);
+  }
+});
 
-
-
-
-
-module.exports= router
-
-
-
-
-    
+module.exports = router;
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,33 +1,32 @@
 import { createContext, useState, useEffect } from "react";
 
-
-export const CurrentUser = createContext()
-
+export const CurrentUser = createContext();
 
 function CurrentUserProvider({ children }) {
-
-    const [currentUser, setCurrentUser] = useState(null)
-    useEffect(() => {
-
-        const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
-            let user = await response.json()
-            setCurrentUser(user)
+  const [currentUser, setCurrentUser] = useState(null);
+  // this useEffect sends fetch request to see if can get a response from authenition.js
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      let response = await fetch(
+        "http://localhost:5000/authentication/profile",
+        {
+            //this is adding an Authorization header
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         }
-        getLoggedInUser()
-    }, [])
-  
+      );
+      let user = await response.json();
+      setCurrentUser(user);
+    };
+    getLoggedInUser();
+  }, []);
 
-   
-
-  
-
-  
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+  return (
+    <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUser.Provider>
+  );
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;
