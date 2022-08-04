407bc975e8bca1011eb7e15a8ed8508d57335e84
RyanHPaez <solosnail@gmail.com>
2022-08-03 20:03:21 -0700

    aws

--- backend/config/config.js
+++ backend/config/config.js
@@ -10,16 +10,18 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": "Solo!nail310",
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DB_NAME,
+    "host": process.env.RDS_HOSTNAME,
+    "port": process.env.RDS_PORT,
+    "dialect": "postgres"
+  },
+
 }
--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require ('path')
 const app = express();
 const cookieSession = require('cookie-session')
+
+
 
 // Express Settings
 app.use(cookieSession({
@@ -30,6 +33,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
 app.use('/authentication', require('./controllers/authentication'))
+
+// serve static front end in production mode--------------------------
+// if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+// }
+
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
